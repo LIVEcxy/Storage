031e9ced0c29adf33f898cd88b83e8655e7f7be2
liusimin <liusimin@richers.com.cn>
2020-03-25 20:15:34 +0800

    支持同步存储

--- storage.js
+++ storage.js
@@ -1,20 +1,20 @@
-/*
- * @Description: local和session工具
- * @Version: 1.0
- * @Autor: liusm
- * @Date: 2020-03-16 15:50:11
- * @LastEditors: liusm
- * @LastEditTime: 2020-03-25 09:04:01
- */
 'use strict'
 
+const _private = Symbol('private');
+const _getType = Symbol('getType');
+const _checkParams = Symbol('checkParams');
+
 export default class Storage {
+
+    Autor   = 'liusm';
+    Version = '1.0.1';
 
     constructor() {
         if(!window) return console.warn('not window')
     }
 
-    static getType(value = undefined) {
+    [_getType](value = undefined) {
+
         if (typeof value !== `object`) {
             return typeof value;
         } else {
@@ -23,11 +23,28 @@ export default class Storage {
             if (types) return types.slice(8, -1);
             throw `unknown type: ${types}, value:${value}`;
         }
+
+    }
+
+    [_checkParams](value) {
+
+        if(value.length < 2){
+            console.warn('key and value  is not defined')
+            return false
+        }
+
+        if(this[_getType](value[0]) !== 'String'){
+            console.warn('key is not String')
+            return false
+        }
+
+        return true
+
     }
 
     modify(object, modifyKey, key, value, type) {
 
-        if(Storage.getType(object) !== 'Object' && Storage.getType(object) !== 'Array') {
+        if(this[_getType](object) !== 'Object' && this[_getType](object) !== 'Array') {
             throw "modify's item not object or Array";
         }else{
             object[key] = value;
@@ -44,8 +61,26 @@ export default class Storage {
 
     }
 
-    setLocal(key,value) {
-        localStorage.setItem(key, JSON.stringify(value))
+    setLocal(key, value, option) {
+        const that = this;
+
+        if(!this[_checkParams](option)) return 'parmas Error'
+
+        if(option[2] && option[2].sync === true){
+
+            return new Promise((resolve, reject) => {
+                localStorage.setItem(option[0], JSON.stringify(option[1]));
+
+                if(that.getSession(option[0])){
+                    resolve(that.getSession(option[0]))
+                }else{
+                    reject(`${option[0]} set Error`)
+                }
+            })
+
+        }else{
+            return localStorage.setItem(option[0], JSON.stringify(option[1]));
+        }
     }
 
     getLocal(key) {
@@ -65,14 +100,33 @@ export default class Storage {
         localStorage.clear();
     }
 
-    
 
 
-    setSession(key, value) {
-        sessionStorage.setItem(key, JSON.stringify(value))
+    setSession(...option) {
+        const that = this;
+
+        if(!this[_checkParams](option)) return 'parmas Error'
+
+        if(option[2] && option[2].sync === true){
+
+            return new Promise((resolve, reject) => {
+                sessionStorage.setItem(option[0], JSON.stringify(option[1]));
+
+                if(that.getSession(option[0])){
+                    resolve(that.getSession(option[0]))
+                }else{
+                    reject(`${option[0]} set Error`)
+                }
+            })
+
+        }else{
+            return sessionStorage.setItem(option[0], JSON.stringify(option[1]));
+        }
+
     }
 
     getSession(key) {
+        this[_private]();
         return JSON.parse(sessionStorage.getItem(key));
     }
 
@@ -89,7 +143,9 @@ export default class Storage {
         sessionStorage.clear();
     }
 
-
+    [_private]() {
+        console.log('private!')
+    }
 
 }
 
